47091e7fa673a7909e86e0a2c61bfc5d7c45b023
Tobias <tobias.lehman@elev.ga.ntig.se>
2021-02-17 10:45:16 +0100

    sstart

---
+++ API/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RestClient client = new RestClient("https://pokeapi.co/api/v2/");
+
+            RestRequest request = new RestRequest("pokemon/totodile");
+
+            request.AddParameter("uid", "ANMA0000032338");
+
+            IRestResponse response = client.Get(request);
+
+            //string content = response.Content;
+
+            //System.Console.WriteLine(content);
+
+            pokemon totodile = JsonConvert.DeserializeObject<pokemon>(response.Content);
+
+            Console.WriteLine(totodile.weight);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
